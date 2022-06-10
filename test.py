486776b385e875770dcd98b7614d99cb33b83e72
herbert <herbertsouza@employer.com.br>
2022-06-10 11:08:25 -0300

    Pyautogui

--- test.py
+++ test.py
@@ -1,60 +1,31 @@
 
-from selenium.common.exceptions import TimeoutException
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.common.keys import Keys
-from selenium import webdriver
 from selenium import webdriver
 import time
-
-# import Alert
-
+import pyautogui
 
 path = "C:\Program Files (x86)\chromedriver.exe"
 
-driver = webdriver.Chrome(path)
-options = webdriver.ChromeOptions()
-options.add_argument('acceptSslCerts')
+driver = webdriver.Chrome(executable_path=path)
 
 driver.maximize_window()
 
 driver.get(
     "https://sso.acesso.gov.br/login?client_id=login.esocial.gov.br&authorization_id=18148b9be79")
 
-try:
-    button = driver.find_element_by_tag_name('button')
-except TimeoutException:
-    print("no button")
+time.sleep(3)
+
+button = driver.find_element_by_tag_name('button')
 
 button.click()
 
-buttonCertificadoDigital = driver.find_element_by_link_text(
-    "Seu certificado digital")
+buttonCertificadoDigital = pyautogui.locateOnScreen(
+    "LoginCertificado.png", grayscale=True)
 
+pyautogui.click(buttonCertificadoDigital)
 
-driver._switch_to.default_content()
-buttonCertificadoDigital.click()
-driver._switch_to.default_content()
+time.sleep(3)
+
+pyautogui.press("enter")
 
 
 time.sleep(10)
-actions = ActionChains(driver)
-actions.send_keys(Keys.ENTER)
-print("enter")
-actions.perform()
-
-try:
-
-    alert = driver.switchTo().alert()
-    print("OOOOOOOOOO", alert)
-    alert.accept()
-
-    actions = ActionChains(driver)
-    actions.send_keys(Keys.ENTER)
-    print("enter")
-    actions.perform()
-
-    print("alert accepted")
-except TimeoutException:
-    print("no alert")
